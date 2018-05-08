699e0a98a89586d359d277a1ca5f8dc186416735
Brett Leibowitz <brettleibo@gmail.com>
2018-05-08 01:59:57 -0400

    let

--- webServer/server.js
+++ webServer/server.js
@@ -172,7 +172,7 @@ io.on('connect', socket => {
        
       let outputFileStream = fs.WriteStream('output.wav');
 
-      let micStatus = 'on';
+      micStatus = 'on';
        
       micInputStream.pipe(outputFileStream);
        

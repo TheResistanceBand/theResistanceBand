d3ea684996a3c37d6bfede202aa79f46bb6d4dfc
Brett Leibowitz <brettleibo@gmail.com>
2018-05-08 01:06:26 -0400

    more mic

--- webServer/server.js
+++ webServer/server.js
@@ -110,30 +110,30 @@ micInputStream.on('error', function(err) {
     cosole.log("Error in Input Stream: " + err);
 });
  
-micInputStream.on('startComplete', function() {
-    console.log("Got SIGNAL startComplete");
-    setTimeout(function() {
-            micInstance.pause();
-    }, 5000);
-});
+// micInputStream.on('startComplete', function() {
+//     console.log("Got SIGNAL startComplete");
+//     setTimeout(function() {
+//             micInstance.pause();
+//     }, 5000);
+// });
     
-micInputStream.on('stopComplete', function() {
-    console.log("Got SIGNAL stopComplete");
-});
+// micInputStream.on('stopComplete', function() {
+//     console.log("Got SIGNAL stopComplete");
+// });
     
-micInputStream.on('pauseComplete', function() {
-    console.log("Got SIGNAL pauseComplete");
-    setTimeout(function() {
-        micInstance.resume();
-    }, 5000);
-});
+// micInputStream.on('pauseComplete', function() {
+//     console.log("Got SIGNAL pauseComplete");
+//     setTimeout(function() {
+//         micInstance.resume();
+//     }, 5000);
+// });
  
-micInputStream.on('resumeComplete', function() {
-    console.log("Got SIGNAL resumeComplete");
-    setTimeout(function() {
-        micInstance.stop();
-    }, 5000);
-});
+// micInputStream.on('resumeComplete', function() {
+//     console.log("Got SIGNAL resumeComplete");
+//     setTimeout(function() {
+//         micInstance.stop();
+//     }, 5000);
+// });
  
 micInputStream.on('silence', function() {
     console.log("Got SIGNAL silence");
